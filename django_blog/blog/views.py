08aed5d03e24a91482ab11c93ba489c73c9b0337
Tamagne <sosina14@gmail.com>
2024-12-02 12:26:17 +0300

    Implementing the Blog's User Authentication System

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,7 +2,8 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from django.contrib.auth.decorators import login_required
-
+from django.contrib import messages
+from .forms import ProfileUpdateForm
 
 def register(request):
     if request.method == 'POST':
@@ -20,3 +21,15 @@ def register(request):
 def profile(request):
     return render(request, 'blog/profile.html')
 
+
+def profile(request):
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('profile')
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+
+    return render(request, 'blog/profile.html', {'form': form})
